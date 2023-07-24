09df5be6afb92000baa9af81a8b07120f8096984
AhmedAmrhashem <ahmedamrhashem@gmail.com>
2023-07-24 14:21:16 -0700

    Midnight commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
+ *swap_int: swaps integers
  *
- *
- *
- *
+ * tmp: a temperoary container to help with the switch
+ * *a: the first argument
+ * *b: the second argument
  */
 void swap_int(int *a, int *b)
 {
 	int tmp;
-	
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
