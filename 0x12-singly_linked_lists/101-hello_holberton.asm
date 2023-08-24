a3cff0bd913796268be57eafdc17f76d4905e469
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-08-24 10:28:34 -0700

    holo

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,36 +1,18 @@
-.LC0:
-        .string "Hello, Holberton"
-        .text
-        .globl  main
-        .type   main, @function
+section .data
+    message db 'Hello, Holberton', 0xA ; 0xA is the ASCII code for a new line character
+
+section .text
+    global main
+
+extern printf
+
 main:
-.LFB0:
-        .cfi_startproc
-        endbr64
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        leaq    .LC0(%rip), %rdi
-        movl    $0, %eax
-        call    printf@PLT
-        movl    $0, %eax
-        popq    %rbp
-        .cfi_def_cfa 7, 8
-        ret
-        .cfi_endproc
-.LFE0:
-        .size   main, .-main
-        .ident  "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-        .section        .note.GNU-stack,"",@progbits
-        .section        .note.gnu.property,"a"
-        .align 8
-        .long    1f - 0f
-        .long    4f - 1f
-        .long    5
-0:
-        .string  "GNU"
-1:
-        .align 8
-        .long    0xc0000002
+    sub rsp, 8 ; Align stack pointer
+
+    mov edi, message ; Pass the address of the message string to printf
+    xor eax, eax ; Clear EAX register
+    call printf ; Call the printf function
+
+    add rsp, 8 ; Restore stack pointer
+    mov eax, 0 ; Return 0 from main
+    ret
