15b53d8f9a6ea1ef2e145f31783052bc62504320
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-08-24 10:00:53 -0700

    hrrll

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void before_main() __attribute__((constructor));
+/**
+ * before_main: prints a string before main
+ * Retrun: void 
+ */
+void before_main() {
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+void free_list(list_t *head)
+{
+	list_t *remove = head;
+
+	while (head)
+	{
+
