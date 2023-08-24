9c65fd3f4ac51f99042cf988e0d8bc06951dde4d
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-08-24 10:22:25 -0700

    printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-void before_main() __attribute__((constructor));
+#include "lists.h"
+
 /**
  * before_main: prints a string before main
  * Retrun: void 
@@ -7,9 +8,3 @@ void before_main() __attribute__((constructor));
 void before_main() {
 	printf("You're beat! and yet, you must allow,\n
 	I bore my house upon my back!\n");
-
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
-}
