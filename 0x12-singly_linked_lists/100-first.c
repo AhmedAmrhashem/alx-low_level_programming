ee52949d3fec9ff57b5bb334c06c2643a63ee98f
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-08-24 10:17:29 -0700

    ALX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,3 +7,9 @@ void before_main() __attribute__((constructor));
 void before_main() {
 	printf("You're beat! and yet, you must allow,\n
 	I bore my house upon my back!\n");
+
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
+}
