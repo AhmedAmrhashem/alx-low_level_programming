f557dbb904af3d7f8106ed7dd79e03e64530be4e
AhmedAmrhashem <ahmedamrhashm@gmail.com>
2023-11-03 01:50:04 -0700

    checking if correct

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,4 +9,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
+        unsigned long int index;
+        hash_node_t *tmp = NULL;
+
+        index = key_index(key, ht->size);
+
+        if (!(ht->array[index]))
+                return (NULL);
+        else
+        {
+                tmp = ht->array[index];
+                if (!strcmp(tmp->key, key))
+                {
+                        return (tmp->value);
+                }
+                tmp = tmp->next;
+        }
+        return (NULL);
+}	
